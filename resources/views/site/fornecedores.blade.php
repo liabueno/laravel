dd4d3192816cf72508bbc6b2177b0345b4f1ca5f
liabueno <juliavbueno13@gmail.com>
2024-04-15 15:20:26 -0300

    .

--- resources/views/site/fornecedores.blade.php
+++ resources/views/site/fornecedores.blade.php
@@ -35,7 +35,15 @@
 
     <br>
 
-    @dd($fornecedores)
+    <!-- @dd($fornecedores) -->
+    
+    @if (count($fornecedores) > 0 && count($fornecedores) < 10) 
+        <h3>Há alguns fornecedores cadastrados</h3>
+    @elseif (count($fornecedores) > 10)
+        <h3>Há muitos fornecedores cadastrados</h3>
+    @else
+        <h3>Não há fornecedores cadastrados</h3>
+    @endif
     
 </body>
 </html>
