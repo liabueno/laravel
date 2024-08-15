d94b031989f32c1af467bd18286d7b7a312ba480
liabueno <juliavbueno13@gmail.com>
2024-08-15 20:44:08 -0300

    Fix: coerência com o proj. anterior

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class FornecedoresController extends Controller
 {
     public function fornecedores(){
+
+        // $fornecedores = ['Fornecedor 1'];
+            
+        // $fornecedores = [
+        //     0 => ['nome ' => 'Fornecedor 1', 'status' => 'Ativo'],
+        //     1 => ['nome ' => 'Fornecedor 2', 'status' => 'Inativo'],
+        //     2 => ['nome ' => 'Fornecedor 3', 'status' => 'Ativo'],
+        // ];
+        // return View('site.fornecedores', compact('fornecedores'));
+
         return View('site.fornecedores', ['pagina' => 'Página de ']);
+
+
     }
 }
--- resources/views/site/fornecedores.blade.php
+++ resources/views/site/fornecedores.blade.php
@@ -6,3 +6,27 @@
 @section('conteudo')
     <h1>Fornecedores</h1>
 @endsection
+
+{{-- @dd($fornecedores) --}}
+    {{--  
+    @if (count($fornecedores) > 0 && count($fornecedores) < 10) 
+        <h3>Há alguns fornecedores cadastrados</h3>
+    @elseif (count($fornecedores) > 10)
+        <h3>Há muitos fornecedores cadastrados</h3>
+    @else
+        <h3>Não há fornecedores cadastrados</h3>
+    @endif
+
+    @isset($fornecedores)
+        @foreach($fornecedores as $indice => $fornecedor)
+            Interação: {{ $loop->iteration }}
+            <br/>
+            @if($loop->first)
+                <h1> Primeiro Fornecedor da Lista </h1>
+            @endif
+            Fornecedor: {{ $fornecedor['nome '] }}
+            <br/>
+            Status: {{ $fornecedor['status'] ?? 'Sem Status definido!' }}
+            <hr><br/>
+        @endforeach  
+    --}}
